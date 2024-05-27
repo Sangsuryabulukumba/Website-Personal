a468c8908b7737ed3f42a6b639de84fa92ab4ae2
Sangsuryabulukumba <sangsuryabulukumba01@gmail.com>
2024-05-27 04:33:45 -0700

    Add files via upload

---
+++ admin/beranda.php
@@ -0,0 +1,16 @@
+<?php
+	require_once "view/header.php";
+?>
+
+	<aside>
+		<div id="abc">
+			<p>Selamat Datang Admin </p>
+		</div>
+
+		<div><img src="../gambar/ADMIN.png" width="100%" height="70%"></div>
+		
+	</aside>
+
+<?php
+	require_once "view/footer.php"
+?>
---
+++ admin/view/footer.php
@@ -0,0 +1,5 @@
+	<footer>
+		<h6> &copy; copyright @ <?= date('Y'); ?> <span>HOTEL SANG SURYA BULUKUMBA BERBASIS SYARIAH</span> | SISTEM INFORMASI_2019 </h6>
+	</footer>
+</body>
+</html>
